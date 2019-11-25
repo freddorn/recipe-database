1851ca563c546276f238513867ea355ed09dedf4
Fred Dorn <freddorn1@gmail.com>
2019-11-25 19:07:08 +0000

    connect to mongodb

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, request
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
+from pymongo import MongoClient
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'recipedb'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+app.config['MONGO_DBNAME'] = 'recipedb'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World Testing'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", tasks=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
