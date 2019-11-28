30d27fe4f7967259606df891cb9395044b412c1e
Fred Dorn <freddorn1@gmail.com>
2019-11-28 00:59:27 +0000

    change app route in app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template("all_recipes.html", tasks=mongo.db.recipes.find())
+    return render_template("all_recipes.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
