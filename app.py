fd104e95671683fc00e43d7e43bc00860ad1abef
Fred Dorn <freddorn1@gmail.com>
2019-11-29 20:48:10 +0000

    refactor app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, session
+from flask import Flask, render_template, redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from pymongo import MongoClient
 from pymongo import ASCENDING
 from pymongo import DESCENDING
 from pymongo import TEXT
+
 
 app = Flask(__name__)
 
@@ -15,16 +16,31 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI', 'mongodb://localhost')
 mongo = PyMongo(app)
 recipes = mongo.db.recipes
 
+
 mongo.db.recipes.create_index([('$**', 'text')])
 mongo.db.users.create_index([('$**', 'text')])
 
+"""List either all recipes in the database or search results"""
+
 
 @app.route('/')
-@app.route('/get_recipes')
-def get_recipes():
-    return render_template("all_recipes.html",
+@app.route('/all_recipes', methods=['GET', 'POST'])
+def all_recipes():
+
+    return render_template('all_recipes.html',
                            recipes=recipes.find().sort('recipe_name', 1),
                            count=recipes.count())
+
+    """List recipes by category"""
+
+
+@app.route('/all_recipes_by_cat/<category_name>', methods=['GET', 'POST'])
+def all_recipes_by_cat(category_name):
+
+    results = recipes.find(
+        {'category_name': category_name}).sort('recipe_name', 1)
+    return render_template('all_recipes.html',
+                           recipes=results, count=results.count())
 
 
 if __name__ == '__main__':
