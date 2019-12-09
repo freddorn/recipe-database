6f89c9a1bb5e84224865b1f49467dfd81b10ba13
Fred Dorn <freddorn1@gmail.com>
2019-12-09 20:39:50 +0000

    move javascript to a separate js page

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+    $('select').material_select();
+    $(".button-collapse").sideNav();
+    $('.materialize-textarea').trigger('autoresize');
+});
